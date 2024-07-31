533b3a0f93f2ed3f49b0737037205642db3d3aa3
Luis Rosales <conejoconsal@hotmail.com>
2024-07-30 19:30:18 -0600

    estilos

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
    return(
-      <footer className="flex justify-center w-full items-center fixed bg-gray-500 bottom-0" >
+      <footer className="flex justify-center w-full items-center fixed bg-gray-500 bottom-0 h-1/5" >
          Hola footer</footer>
    )
 }
