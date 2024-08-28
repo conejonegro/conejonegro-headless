1b79754d4186ee4d834a84c66c03767a41521418
Luis Rosales <conejoconsal@hotmail.com>
2024-08-27 18:53:36 -0600

    e

--- src/apollo-client.jsx
+++ src/apollo-client.jsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://conejonegro.local/graphql', // Reemplaza con la URL de tu endpoint de WPGraphQL
+  uri: 'https://mcseguros.com.mx/portfolio/graphql', // Reemplaza con la URL de tu endpoint de WPGraphQL
   cache: new InMemoryCache(),
 });
 
