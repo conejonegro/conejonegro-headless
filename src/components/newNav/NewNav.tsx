4d13212f42a9dd0b4e34fcbd00162d5814d0fe4f
Luis Rosales <conejoconsal@hotmail.com>
2024-11-19 18:02:30 -0600

    Link added to structure

--- src/components/newNav/NewNav.tsx
+++ src/components/newNav/NewNav.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { CiMenuBurger } from "react-icons/ci";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import Image from "next/image";
 import Link from "next/link";
@@ -32,15 +28,21 @@ export default function NewNav() {
         </Link>
          { menuItems.map((menuItem) =>  {
             return(
-               <li key={menuItem.label}>{menuItem.label}</li>
+              <li key={menuItem.label}>
+                <Link href={menuItem.href}>
+                  {menuItem.label}
+                </Link>
+              </li>
             )
          })}
        
       </ul>
+      { /*  Hamburguer Button */ }
       <button aria-label="Toggle navigation menu" aria-expanded={isOpen}  className= "z-10 mr-8 w-full flex justify-between items-center md:hidden">
       <Link href="/"> <Image src="/static/blackRabbit.png" alt="image" width={80} height={80}/> </Link>
         {isOpen ? <IoMdClose onClick={clicked} /> : <CiMenuBurger onClick={clicked} />}
       </button>
+      { /*  Dropdown Menu */ }
       <div
         className={`${
           isOpen ? "max-h-screen mt-4 flex overflow-hidden transition-all z-10 duration-500 ease-in-out absolute top-16 left-0 w-full bg-slate-600 text-white  p-4 md:hidden" : "hidden overflow-hidden transition-all duration-500 ease-in-out absolute top-16 left-0 w-full bg-black text-white p-4 md:hidden"
@@ -50,9 +52,9 @@ export default function NewNav() {
           {menuItems.map((menuItem) => (
             <li key={menuItem.label} className="py-2">
              
-              <a href={menuItem.href} className="hover:text-gray-400">
+              <Link href={menuItem.href} className="hover:text-gray-400">
                 {menuItem.label}
-              </a>
+              </Link>
               <hr className="mt-4 border-slate-50"></hr>
             </li>
           ))}
